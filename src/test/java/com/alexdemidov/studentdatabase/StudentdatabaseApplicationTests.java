1af80760d05a46b4159eff190b19bd29be93bb44
alex <alexdeveloper123@gmail.com>
2022-04-21 15:03:15 +0300

    Beginning of the project

---
+++ src/test/java/com/alexdemidov/studentdatabase/StudentdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexdemidov.studentdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
